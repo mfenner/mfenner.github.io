ec22ee839f08d296727313af197720cf3af9a11d
Martin Fenner <mf@martinfenner.org>
2013-07-29 11:53:03 +0200

    Simplified Pandoc setup, prepare for other formats

--- _plugins/jekyll_asset_pipeline.rb
+++ _plugins/jekyll_asset_pipeline.rb
@@ -24,28 +24,4 @@ module JekyllAssetPipeline
       return CoffeeScript.compile(@content)
     end
   end
-  
-  class CssCompressor < JekyllAssetPipeline::Compressor
-      require 'yui/compressor'
-
-      def self.filetype
-        '.css'
-      end
-
-      def compress
-        return YUI::CssCompressor.new.compress(@content)
-      end
-    end
-
-    class JavaScriptCompressor < JekyllAssetPipeline::Compressor
-      require 'yui/compressor'
-
-      def self.filetype
-        '.js'
-      end
-
-      def compress
-        return YUI::JavaScriptCompressor.new(munge: true).compress(@content)
-      end
-    end
 end
