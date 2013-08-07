d514757365fe8952fdabebead2cdc563ccd68746
Martin Fenner <mf@martinfenner.org>
2013-08-07 14:09:18 +0200

    Use string instead of regex for adding markdown to URL

--- _plugins/jekyll_scholar.rb
+++ _plugins/jekyll_scholar.rb
@@ -6,7 +6,7 @@ module MarkdownFilter
     def apply(value)
       url = value.to_s.slice(URI.regexp(['http','https','ftp']))
       return value unless url
-      value.to_s.gsub(/(#{url})/, '[\1](\1)')
+      value.to_s.gsub(url, "[#{url}](#{url})")
     end
   end
 end
