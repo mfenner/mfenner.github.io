f3b4bc97af7c0c231a20828011948bb14efba4fc
Martin Fenner <mf@martinfenner.org>
2013-08-07 12:46:08 +0200

    More flexible URL substitution pattern

--- _plugins/jekyll_scholar.rb
+++ _plugins/jekyll_scholar.rb
@@ -5,7 +5,8 @@ module MarkdownFilter
   class Markdown < BibTeX::Filter
     def apply(value)
       url = value.to_s.slice(URI.regexp(['http','https','ftp']))
-      value = url ? "[#{url}](#{url})" : value
+      return value unless url
+      value.to_s.gsub(/(#{url})/, '[\1](\1)')
     end
   end
 end
