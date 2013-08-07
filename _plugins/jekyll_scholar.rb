6180725ddf38ae396fe5b8dec5ab376ac0b5c641
Martin Fenner <mf@martinfenner.org>
2013-08-07 14:46:02 +0200

    Simplified code

--- _plugins/jekyll_scholar.rb
+++ _plugins/jekyll_scholar.rb
@@ -4,9 +4,7 @@ require 'uri'
 module MarkdownFilter
   class Markdown < BibTeX::Filter
     def apply(value)
-      url = value.to_s.slice(URI.regexp(['http','https','ftp']))
-      return value unless url
-      value.to_s.gsub(url, "[#{url}](#{url})")
+      value.to_s.gsub(URI.regexp(['http','https','ftp'])) { |c| "[#{$&}](#{$&})" }
     end
   end
 end
